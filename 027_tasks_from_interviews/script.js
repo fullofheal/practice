3857d0cab5b783e9dde8a1c6e62ce22a3e1dd5f7
Misha <zelenskiymisha@gmail.com>
2022-04-02 17:39:25 +0300

    tasks_from_interviews

---
+++ 027_tasks_from_interviews/script.js
@@ -0,0 +1,62 @@
+'use strict';
+
+// 1) Какое будет выведено значение: 
+// let x = 5; 
+// alert(x++); // ++x - 6
+// 5
+
+// Чему равно такое выражение: [ ] + false - null + true ?
+// String // wrong answer // right answer - NaN.
+
+// const a = [] + false - false;
+// console.log(a);
+// Что выведет этот код: 
+// let y = 1; 
+// let x = y = 2; alert(x);
+// 2
+
+// Чему равна сумма [ ] + 1 + 2?
+// 12
+// const b = [] + 1 + 2;
+// console.log(typeof(b));
+// console.log(b);
+
+// Что выведет этот код: 
+// alert( "1"[0] );
+// 10
+
+// Чему равно 2 && 1 && null && 0 && undefined ?
+// undefined // wrong answer. correcty answer - null. "И" всегда запинается на лжи.
+
+// Есть ли разница между выражениями? !!( a && b ) и (a && b)?
+// yes - !! - boolean
+
+// Что выведет этот код: 
+// Или запинается на правде 
+// alert( null || 2 && 3 || 4 );
+// null || 2 && 3 || 4
+// 3
+
+// a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ?
+// no, but idk why 
+// const a = [1, 2, 3];
+// const b = [1, 2, 3];
+
+// if(a == b) {
+//     console.log('Hello');
+// } else {
+//     console.log('wrong');
+// }
+
+// Что выведет этот код: 
+// alert( +"Infinity" ); 
+// Infinity
+
+// Верно ли сравнение: "Ёжик" > "яблоко"?
+// console.log("Ёжик" > "яблоко");
+// false
+
+// Чему равно 0 || "" || 2 || undefined || true || falsе ?
+// 2
+
+// console.log('y' > '1');
